88ba9bf21240762ac310cb4ea34a5c37ae44d8b4
Pabak Dev <53967227+pabak-dev@users.noreply.github.com>
2026-01-30 21:31:41 +0600

    Button VFX

--- Assets/Code/ButtonFX.cs
+++ Assets/Code/ButtonFX.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using DG.Tweening;
 
 [RequireComponent(typeof(Button))]
-public class ButtonFX : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler
+public class ButtonFX : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
 {
     [Header("Audio Clips")]
     [SerializeField] private AudioClip hoverSound;
@@ -11,12 +12,15 @@ public class ButtonFX : MonoBehaviour, IPointerEnterHandler, IPointerClickHandle
 
     [Header("Settings")]
     [SerializeField] private bool ignoreIfNotInteractable = true;
+    [SerializeField] private float hoverScale = 1.1f;
 
     private Button _button;
+    private Vector3 _originalScale;
 
     private void Awake()
     {
         _button = GetComponent<Button>();
+        _originalScale = transform.localScale;
     }
 
     public void OnPointerEnter(PointerEventData eventData)
@@ -43,13 +47,40 @@ public class ButtonFX : MonoBehaviour, IPointerEnterHandler, IPointerClickHandle
         PlayClickVFX();
     }
 
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (ignoreIfNotInteractable && _button != null && !_button.interactable) return;
+        
+        PlayExitVFX();
+    }
+
     private void PlayHoverVFX()
     {
-        // anims
+
+        transform.DOKill(); 
+
+        transform.DOScale(_originalScale * hoverScale, 0.15f)
+            .SetEase(Ease.OutBack)
+            .SetUpdate(true); 
+    }
+
+    private void PlayExitVFX()
+    {
+        transform.DOKill();
+
+        transform.DOScale(_originalScale, 0.15f)
+            .SetEase(Ease.OutQuad)
+            .SetUpdate(true);
     }
 
     private void PlayClickVFX()
     {
-        // anims
+        transform.DOKill();
+
+        transform.DOPunchScale(_originalScale * -0.1f, 0.15f, 10, 1)
+            .SetUpdate(true)
+            .OnComplete(() => {
+                transform.localScale = _originalScale; 
+            });
     }
 }
